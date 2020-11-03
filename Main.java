a7998fae2582be78472d4fc5d41deb95431913c6
felipearosr <felipe.aros.r@gmail.com>
2020-11-02 22:32:25 -0400

    Added Print level Recursive

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
-import java.util.Scanner;
 import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 class Node {
     int key;
@@ -113,14 +115,41 @@ class BinarySearchTree {
         }
         return root;
     }
-    int minValue(Node root)  { 
+
+    int minValue(Node root) {
         int minval = root.key;
-        while (root.left != null)  { 
-            minval = root.left.key; 
-            root = root.left; 
-        } 
-        return minval; 
-    } 
+        while (root.left != null) {
+            minval = root.left.key;
+            root = root.left;
+        }
+        return minval;
+    }
+
+    void levelO() {
+        levelOrder(root);
+    }
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();  
+        if (root == null)
+            return res;
+        Queue<Node> queue = new LinkedList<>();  
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();  
+            int cnt = queue.size();  
+            for (int i = 0; i < cnt; i++) {
+                Node node = queue.poll();
+                level.add(node.key);
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }  
+            res.add(level);   
+        }  
+        return res;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -131,6 +160,7 @@ class BinarySearchTree {
         tree.preOrderTraversal();
         tree.inOrderTraversal();
         tree.postOrderTraversal();
+        System.out.println(levelO());
         scanner.close();
     }
 }
